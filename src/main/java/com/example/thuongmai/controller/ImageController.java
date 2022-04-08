f94b353d845642be6b181302690ede7846218b4f
ThaiVu54 <thai01654369440@gmail.com>
2022-04-08 23:26:40 +0700

    done image controller

---
+++ src/main/java/com/example/thuongmai/controller/ImageController.java
@@ -0,0 +1,11 @@
+package com.example.thuongmai.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/images")
+public class ImageController {
+}
